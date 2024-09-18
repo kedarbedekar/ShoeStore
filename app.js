00381afa1a3d7fb468973438c93542af0af1582a
kedarbedekar <kedar.111bedekar@gmail.com>
2024-09-18 13:52:20 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-let cart = [];
-
-function addToCart(productName, price) {
-    cart.push({ productName, price });
-    alert(productName + " added to cart!");
-    console.log(cart); // Cart can be displayed dynamically in future
-}
