a68634b376c768c36fce8572abadcc6fbb2e5d2f
kedarbedekar <kedar.111bedekar@gmail.com>
2024-10-11 19:23:54 -0400

    Update app.js

--- app.js
+++ app.js
@@ -84,3 +84,32 @@ function clearCart() {
     saveCart(); // Save the empty cart to localStorage
     document.getElementById('cart-items').innerHTML = ''; // Clear cart items from the display
 }
+
+// JavaScript for Modal functionality
+
+// Get the modal
+var modal = document.getElementById("shop-now-modal");
+
+// Get the button that opens the modal
+var btn = document.querySelector(".shop-now");
+
+// Get the <span> element that closes the modal
+var span = document.querySelector(".modal .close");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
